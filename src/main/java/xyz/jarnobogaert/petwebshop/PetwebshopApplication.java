b4203f9bc8f53cb3b06ae428dce5538bfbf6cafa
Jarno Bogaert <jarnobogaert9@gmail.com>
2020-12-17 14:34:07 +0100

    Initialize project

---
+++ src/main/java/xyz/jarnobogaert/petwebshop/PetwebshopApplication.java
@@ -0,0 +1,13 @@
+package xyz.jarnobogaert.petwebshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetwebshopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PetwebshopApplication.class, args);
+    }
+
+}
---
+++ src/test/java/xyz/jarnobogaert/petwebshop/PetwebshopApplicationTests.java
@@ -0,0 +1,13 @@
+package xyz.jarnobogaert.petwebshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetwebshopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
