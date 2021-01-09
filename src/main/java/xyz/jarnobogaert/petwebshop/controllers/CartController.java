ef6b0ed3db92496a41c56bb291ba002155fe5779
Jarno Bogaert <jarnobogaert9@gmail.com>
2021-01-09 16:46:53 +0100

    Feat: show the products from a user's cart

--- src/main/java/xyz/jarnobogaert/petwebshop/controllers/CartController.java
+++ src/main/java/xyz/jarnobogaert/petwebshop/controllers/CartController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.access.prepost.PreInvocationAttribute;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -15,6 +16,7 @@ import xyz.jarnobogaert.petwebshop.repositories.ProductRepo;
 import xyz.jarnobogaert.petwebshop.repositories.UserRepo;
 
 import java.security.Principal;
+import java.util.List;
 import java.util.Optional;
 
 @Controller
@@ -25,22 +27,28 @@ public class CartController {
     ProductRepo productRepo;
 //    @PreAuthorize("isAuthenticated()")
     @GetMapping("/cart")
-    public String cart(@RequestParam(value = "productId", required = false) String productId, Principal principal) {
+    public String cart(@RequestParam(value = "productId", required = false) String productId, Principal principal, ModelMap modelMap) {
+        // Get user based on logged in user his/her username
+        User user = userRepo.findByUsername(principal.getName()).get();
+
         if (productId == null) {
             System.out.println("============ GET: /cart ============");
+            List<Product> products = user.getProducts();
+
+            // Set model attribute so we can access it in our template
+            modelMap.addAttribute("products", products);
+
             return "cart";
         }
         System.out.println("============ Add to card ============");
         System.out.println(productId);
 
-        // TODO add product id to a list of a user so we can track products in the cart
-        // Get user based on logged in user his/her username
-        User user = userRepo.findByUsername(principal.getName()).get();
-
         // Get the product that we need to add to the user his/her cart
-        Product product = productRepo.findById(Integer.parseInt(productId)).get();
+        Optional<Product> product = productRepo.findById(Integer.parseInt(productId));
 
-        user.getProducts().add(product);
+        // Add product id to the list of products, make sure there is a product so avoid exceptions
+        if (product.isPresent())
+            user.getProducts().add(product.get());
 
         // Save the changes
         userRepo.save(user);
