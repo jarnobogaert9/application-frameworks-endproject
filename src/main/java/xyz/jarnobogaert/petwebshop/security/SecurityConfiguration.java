f3d482b290c811738c45ace572e2c6f38e62357e
Jarno Bogaert <jarnobogaert9@gmail.com>
2021-01-14 14:18:02 +0100

    Remove unused import statements

--- src/main/java/xyz/jarnobogaert/petwebshop/security/SecurityConfiguration.java
+++ src/main/java/xyz/jarnobogaert/petwebshop/security/SecurityConfiguration.java
@@ -12,11 +12,6 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
-
 @Configuration
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled=true)
